e4545fc1ab6cbe8ebfeb9c65347718083b36c8b8
jjaplot <62018438+jjaplot@users.noreply.github.com>
2020-03-10 18:09:31 +0100

    Update app.py

--- app.py
+++ app.py
@@ -18,5 +18,15 @@ def graph():
     return render_template('graph.html')
 
 
+@application.route('/chart-data')
+def chart_data():
+    def generate_random_data():
+        while True:
+            json_data = json.dumps(
+                {'time': datetime.now().strftime('%Y-%m-%d %H:%M:%S'), 'value': random.random() * 100})
+            yield f"data:{json_data}\n\n"
+            time.sleep(1)
+
+    return Response(generate_random_data(), mimetype='text/event-stream')
 
 if __name__ == '__main__': app.run(debug=True)
